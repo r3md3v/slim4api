9ceb7f59e9a4d6fccc73badc129409b52ceffa61
edgd1er <edgd1er@hotmail.com>
2020-08-28 14:33:10 +0200

    check ok, validation ko

--- public/js/veriform.js
+++ public/js/veriform.js
@@ -1,16 +1,13 @@
 function verifForm(arrayFields, msg) {
     // Alert and color if fields marked with asterisk are empty
     var nbVerif = 0;
-    var elt;
     var warning = false;
     arrayFields.forEach(function (item) {
-        //console.log('item: ' + item);
-        elt = document.getElementsByName(item)[0]
-        if (elt.value == '') {
-            elt.style.backgroundColor = '#FF5555';
+        if (item.value == '') {
+            item.style.backgroundColor = '#FF5555';
             warning = true;
         } else {
-            elt.style.backgroundColor = '';
+            item.style.backgroundColor = '';
             nbVerif++;
         }
     });
@@ -25,13 +22,11 @@ function verifForm(arrayFields, msg) {
 function verifFormSpace(arrayFields, msg) {
     // Alert and color if fields should not contain space
     var nbVerif = 0;
-    var elt;
     arrayFields.forEach(function (item) {
-        elt = document.getElementsByName(item)[0]
-        if (elt.value.indexOf(" ") != -1) {
-            elt.style.backgroundColor = '#FFAAAA';
+        if (itemt.value.indexOf(" ") != -1) {
+            item.style.backgroundColor = '#FFAAAA';
         } else {
-            elt.style.backgroundColor = '';
+            item.style.backgroundColor = '';
             nbVerif++;
         }
     });
@@ -41,9 +36,8 @@ function verifFormSpace(arrayFields, msg) {
     }
 }
 
-function validateEmail(emailField) {
-    var email = document.getElementsByName(emailField)[0]
-    //console.log('email:' + email.value);
+function validateEmail(email) {
+    console.log('email:' + email.value);
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     var res = re.test(email.value);
     if (!res) {
