fa7b3c0abbcb300be0bb220f52a5ac47fe06c2f3
R3mD3v <remi.dubiez@libertysurf.fr>
2020-09-10 14:45:51 +0200

    Improved source IP detection

--- src/Domain/Login/Service/LoginManager.php
+++ src/Domain/Login/Service/LoginManager.php
@@ -131,18 +131,72 @@ final class LoginManager
     }
 
     /**
-     * Return source IP of client.
+     * Retrieves IP address from request header.
      *
-     * @return sourceip The Srouce IP
+     * @return ipAddress The Source IP
      */
     public function getUserIpAddr()
     {
+        $ipAddress = '';
+        if (!empty($_SERVER['HTTP_CLIENT_IP']) && $this->isValidIpAddress($_SERVER['HTTP_CLIENT_IP'])) {
+            // check for shared ISP IP
+            $ipAddress = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            // check for IPs passing through proxy servers
+            // check if multiple IP addresses are set and take the first one
+            $ipAddressList = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            foreach ($ipAddressList as $ip) {
+                if ($this->isValidIpAddress($ip)) {
+                    $ipAddress = $ip;
+
+                    break;
+                }
+            }
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED']) && $this->isValidIpAddress($_SERVER['HTTP_X_FORWARDED'])) {
+            $ipAddress = $_SERVER['HTTP_X_FORWARDED'];
+        } elseif (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && $this->isValidIpAddress($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) {
+            $ipAddress = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_FORWARDED_FOR']) && $this->isValidIpAddress($_SERVER['HTTP_FORWARDED_FOR'])) {
+            $ipAddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        } elseif (!empty($_SERVER['HTTP_FORWARDED']) && $this->isValidIpAddress($_SERVER['HTTP_FORWARDED'])) {
+            $ipAddress = $_SERVER['HTTP_FORWARDED'];
+        } elseif (!empty($_SERVER['REMOTE_ADDR']) && $this->isValidIpAddress($_SERVER['REMOTE_ADDR'])) {
+            $ipAddress = $_SERVER['REMOTE_ADDR'];
+        }
+
+        return $ipAddress;
+    }
+
+    /**
+     * Is IP address is both a valid, and does not fall within a private network range.
+     *
+     * @param string $ip
+     *
+     * @return bool
+     */
+    public function isValidIpAddress($ip)
+    {
+        if (false === filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Return source IP of client.
+     *
+     * @return ip The Source IP
+     */
+    public function getUserIpAddrLight()
+    {
         // $_SERVER['REMOTE_ADDR'] might not returns the correct IP address of the user in case of Proxy.
+
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-            //ip from share internet
+            // IP from share internet
             $ip = $_SERVER['HTTP_CLIENT_IP'];
         } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            //ip pass from proxy
+            // IP from proxy
             $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
         } else {
             $ip = $_SERVER['REMOTE_ADDR'];
