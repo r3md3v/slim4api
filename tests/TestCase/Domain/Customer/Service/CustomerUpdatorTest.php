3b514a30241f9c01ae2dfd50e2144a0276013978
R3mD3v <remi.dubiez@libertysurf.fr>
2020-09-22 12:39:07 +0200

    Test ok except testUpdateUserOk testUpdateCustomerOk MissingPhone

---
+++ tests/TestCase/Domain/Customer/Service/CustomerUpdatorTest.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace Tests\TestCase\Domain\Customer\Service;
+
+use App\Domain\Customer\Repository\CustomerUpdatorRepository;
+use App\Domain\Customer\Service\CustomerUpdator;
+use App\Exception\ValidationException;
+use PHPUnit\Framework\TestCase;
+use Tests\AppTestTrait;
+
+class CustomerUpdatorTest extends TestCase
+{
+
+    use AppTestTrait;
+
+    public function testUpdateCustomerOk()
+    {
+        $customer = [
+            'cusname' => 'Customer SARL',
+            'address' => '25 rue des arbres',
+            'city' => '59000 Lille',
+            'phone' => '0102030405',
+            'email' => 'john@doe.com',
+        ];
+
+        // Mock the required repository method
+        $this->mock(CustomerUpdatorRepository::class);
+
+        $service = $this->container->get(CustomerUpdator::class);
+        $actual = $service->updateCustomer(1, $customer);
+
+        static::assertSame(1, $actual);
+
+    }
+
+    public function testUpdateCustomerKOCustomerExists(): void
+    {
+        $customer = [
+            'cusname' => 'Customer SARL',
+            'address' => '25 rue des arbres',
+            'city' => '59000 Lille',
+            'phone' => '0102030405',
+            'email' => 'john@doe.com',
+        ];
+
+        // Mock the required repository method
+        $this->mock(CustomerUpdatorRepository::class)->method('customerExists')->with(1, $customer['email'])->willReturn(true);
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('Customer already exists with email [' . $customer['email'] . ']');
+
+		$service = $this->container->get(CustomerUpdator::class);
+        $actual = $service->updateCustomer(1, $customer);
+
+    }
+
+    public function testUpdateCustomerKOFormMissingCusName(): void
+    {
+        $customer = [
+            'cusname' => '',
+            'address' => '25 rue des arbres',
+            'city' => '59000 Lille',
+            'phone' => '0102030405',
+            'email' => 'john@doe.com',
+        ];
+
+        // Mock the required repository method
+        $this->mock(CustomerUpdatorRepository::class);
+        $service = $this->container->get(CustomerUpdator::class);
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('Please check your input.');
+
+        $actual = $service->updateCustomer(1, $customer);
+
+    }
+
+    public function testUpdateCustomerKOFormMissingAddress(): void
+    {
+        $customer = [
+            'cusname' => 'Customer SARL',
+            'address' => '',
+            'city' => '59000 Lille',
+            'phone' => '0102030405',
+            'email' => 'john@doe.com',
+        ];
+
+        // Mock the required repository method
+        $this->mock(CustomerUpdatorRepository::class);
+        $service = $this->container->get(CustomerUpdator::class);
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('Please check your input.');
+
+        $actual = $service->updateCustomer(1, $customer);
+
+    }
+
+    public function testUpdateCustomerKOFormMissingCityName(): void
+    {
+        $customer = [
+            'cusname' => 'Customer SARL',
+            'address' => '25 rue des arbres',
+            'city' => '',
+            'phone' => '0102030405',
+            'email' => 'john@doe.com',
+        ];
+
+        // Mock the required repository method
+        $this->mock(CustomerUpdatorRepository::class);
+
+        $service = $this->container->get(CustomerUpdator::class);
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('Please check your input.');
+
+        $actual = $service->updateCustomer(1, $customer);
+
+    }
+
+    public function testUpdateCustomerOKFormMissingPhone(): void
+    {
+        $customer = [
+            'cusname' => 'Customer SARL',
+            'address' => '25 rue des arbres',
+            'city' => '59000 Lille',
+            'phone' => '',
+            'email' => 'john@doe.com',
+        ];
+
+        // Mock the required repository method
+        $this->mock(CustomerUpdatorRepository::class);
+
+        $service = $this->container->get(CustomerUpdator::class);
+
+        $actual = $service->updateCustomer(1, $customer);
+        static::assertSame(1, $actual);
+
+    }
+
+    public function testUpdateCustomerKOFormMissingEmail(): void
+    {
+        $customer = [
+            'cusname' => 'Customer SARL',
+            'address' => '25 rue des arbres',
+            'city' => '59000 Lille',
+            'phone' => '0102030405',
+            'email' => '',
+        ];
+
+        // Mock the required repository method
+        $this->mock(CustomerUpdatorRepository::class);
+
+        $service = $this->container->get(CustomerUpdator::class);
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('Please check your input.');
+
+        $actual = $service->updateCustomer(1, $customer);
+
+    }
+
+    public function testUpdateCustomerKOFormFormatEmail(): void
+    {
+        $customer = [
+            'cusname' => 'Customer SARL',
+            'address' => '25 rue des arbres',
+            'city' => '59000 Lille',
+            'phone' => '0102030405',
+            'email' => 'john@doe'
+        ];
+
+        // Mock the required repository method
+		$this->mock(CustomerUpdatorRepository::class);
+        $service = $this->container->get(CustomerUpdator::class);
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('Please check your input.');
+
+        $actual = $service->updateCustomer(1, $customer);
+
+    }
+}
---
+++ tests/TestCase/Domain/User/Service/UserUpdatorTest.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace Tests\TestCase\Domain\User\Service;
+
+use App\Domain\User\Repository\UserUpdatorRepository;
+use App\Domain\User\Service\UserUpdator;
+use App\Exception\ValidationException;
+use PHPUnit\Framework\TestCase;
+use Tests\AppTestTrait;
+
+class UserUpdatorTest extends TestCase
+{
+
+    use AppTestTrait;
+
+    public function testUpdateUserOk()
+    {
+        $user = [
+            'username' => 'john.doe',
+            'password' => '1234567',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'john@doe.com',
+            'profile' => 'users customers'
+        ];
+
+        // Mock the required repository method
+        $this->mock(UserUpdatorRepository::class);
+
+        $service = $this->container->get(UserUpdator::class);
+        $actual = $service->updateUser(1, $user);
+
+        static::assertSame(1, $actual);
+
+    }
+
+    public function testUpdateUserKOUserExists(): void
+    {
+        $user = [
+            'username' => 'john.doe',
+            'password' => '1234567',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'john@doe.com',
+            'profile' => 'users customers'
+        ];
+
+        // Mock the required repository method
+        $this->mock(UserUpdatorRepository::class)->method('userExists')->with(1, $user['username'], $user['email'])->willReturn(true);
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('User already exists with name [' . $user['username'] . '] or email [' . $user['email'] . ']');
+
+		$service = $this->container->get(UserUpdator::class);
+        $actual = $service->updateUser(1, $user);
+
+    }
+
+    public function testUpdateUserKOFormMissingUserName(): void
+    {
+        $user = [
+            'username' => '',
+            'password' => '1234567',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'john@doe.com',
+            'profile' => 'users customers'
+        ];
+
+        // Mock the required repository method
+        $this->mock(UserUpdatorRepository::class);
+        $service = $this->container->get(UserUpdator::class);
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('Please check your input.');
+
+        $actual = $service->updateUser(1, $user);
+
+    }
+
+    public function testUpdateUserKOFormMissingPassword(): void
+    {
+        $user = [
+            'username' => 'john.doe',
+            'password' => '',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'john@doe.com',
+            'profile' => 'users customers'
+        ];
+
+        // Mock the required repository method
+        $this->mock(UserUpdatorRepository::class);
+        $service = $this->container->get(UserUpdator::class);
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('Please check your input.');
+
+        $actual = $service->updateUser(1, $user);
+
+    }
+
+    public function testUpdateUserKOFormMissingFirstName(): void
+    {
+        $user = [
+            'username' => 'john.doe',
+            'password' => '1234567',
+            'first_name' => '',
+            'last_name' => 'Doe',
+            'email' => 'john@doe.com',
+            'profile' => 'users customers'
+        ];
+
+        // Mock the required repository method
+        $this->mock(UserUpdatorRepository::class);
+        $service = $this->container->get(UserUpdator::class);
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('Please check your input.');
+
+        $actual = $service->updateUser(1, $user);
+
+    }
+
+    public function testUpdateUserKOFormMissingLastName(): void
+    {
+        $user = [
+            'username' => 'john.doe',
+            'password' => '1234567',
+            'first_name' => 'John',
+            'last_name' => '',
+            'email' => 'john@doe.com',
+            'profile' => 'users customers'
+        ];
+
+        // Mock the required repository method
+        $this->mock(UserUpdatorRepository::class);
+
+        $service = $this->container->get(UserUpdator::class);
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('Please check your input.');
+
+        $actual = $service->updateUser(1, $user);
+
+    }
+
+    public function testUpdateUserKOFormMissingEmail(): void
+    {
+        $user = [
+            'username' => 'john.doe',
+            'password' => '1234567',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => '',
+            'profile' => 'users customers'
+        ];
+
+        // Mock the required repository method
+        $this->mock(UserUpdatorRepository::class);
+
+        $service = $this->container->get(UserUpdator::class);
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('Please check your input.');
+
+        $actual = $service->updateUser(1, $user);
+
+    }
+
+    public function testUpdateUserKOFormMissingProfile(): void
+    {
+        $user = [
+            'username' => 'john.doe',
+            'password' => '1234567',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'john@doe.com',
+            'profile' => ''
+        ];
+
+        // Mock the required repository method
+        $this->mock(UserUpdatorRepository::class);
+
+        $service = $this->container->get(UserUpdator::class);
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('Please check your input.');
+
+        $actual = $service->updateUser(1, $user);
+
+    }
+	
+	public function testUpdateUserKOFormFormatEmail(): void
+    {
+        $user = [
+            'username' => 'john.doe',
+            'password' => '1234567',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'joh@doe',
+            'profile' => 'users customers'
+        ];
+
+        // Mock the required repository method
+        $this->mock(UserUpdatorRepository::class);
+
+        $service = $this->container->get(UserUpdator::class);
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('Please check your input.');
+
+        $actual = $service->updateUser(1, $user);
+
+    }
+}
